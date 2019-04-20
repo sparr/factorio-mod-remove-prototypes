18462eb911776c71b62e47e7a9bac388c09f67e1
Clarence "Sparr" Risher <sparr0@gmail.com>
2019-04-20 13:44:09 -0700

    Update for 0.17, refactor, improve

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,20 +1,24 @@
--- every key of data.raw that contains entities
-local entity_types = { ["accumulator"] = true, ["arithmetic-combinator"] = true, ["artillery-flare"] = true, ["artillery-projectile"] = true, ["artillery-turret"] = true, ["artillery-wagon"] = true, ["assembling-machine"] = true, ["beacon"] = true, ["boiler"] = true, ["capsule"] = true, ["car"] = true, ["cargo-wagon"] = true, ["character-corpse"] = true, ["cliff"] = true, ["combat-robot"] = true, ["constant-combinator"] = true, ["construction-robot"] = true, ["container"] = true, ["corpse"] = true, ["decider-combinator"] = true, ["electric-pole"] = true, ["electric-turret"] = true, ["entity-ghost"] = true, ["explosion"] = true, ["fire"] = true, ["fish"] = true, ["flame-thrower-explosion"] = true, ["fluid-turret"] = true, ["fluid-wagon"] = true, ["flying-text"] = true, ["furnace"] = true, ["gate"] = true, ["generator"] = true, ["heat-pipe"] = true, ["inserter"] = true, ["lab"] = true, ["lamp"] = true, ["land-mine"] = true, ["loader"] = true, ["locomotive"] = true, ["logistic-container"] = true, ["logistic-robot"] = true, ["mining-drill"] = true, ["monolith"] = true, ["offshore-pump"] = true, ["pipe-to-ground"] = true, ["pipe"] = true, ["player-port"] = true, ["player"] = true, ["power-switch"] = true, ["programmable-speaker"] = true, ["projectile"] = true, ["pump"] = true, ["radar"] = true, ["rail-chain-signal"] = true, ["rail-remnants"] = true, ["rail-signal"] = true, ["reactor"] = true, ["roboport"] = true, ["rocket-silo"] = true, ["solar-panel"] = true, ["splitter"] = true, ["storage-tank"] = true, ["straight-rail"] = true, ["tile"] = true, ["transport-belt"] = true, ["turret"] = true, ["underground-belt"] = true, ["unit-spawner"] = true, ["unit"] = true, ["wall"] = true, }
+require("entity-categories")
 
 -- given a recipe and an item, remove that item from the recipe's ingredients and results
 -- remove the recipe if it's left with no results
 function remove_recipe_ingredient_or_result(recipe, item_name)
+  -- just one result?
   if recipe.result == item_name then
+    -- remove the whole recipe if the result matches
     remove_one_prototype("recipe", recipe.name)
     return
   end
+  -- multiple results?
   if recipe.results then
     for i,result in pairs(recipe.results) do
       if result.name == item_name then
+        -- remove matching result(s)
         table.remove(recipe.results, i)
       end
     end
     if #recipe.results == 0 then
+      -- remove the recipe if no results remain
       remove_one_prototype("recipe", recipe.name)
       return
     end
@@ -26,9 +30,11 @@ function remove_recipe_ingredient_or_result(recipe, item_name)
     }) do
     for i,ingredient in pairs(ingredients_table) do
       if ingredient[1] == item_name or ingredient.name == item_name then
+        -- remove ingredients that are this item
         table.remove(ingredients_table, i)
       end
     end
+    -- but don't remove recipes with no ingredients
   end
 end
 
@@ -38,52 +44,44 @@ function remove_technology_unlock(tech, recipe_name)
     for i,effect in pairs(tech.effects) do
       if effect.type == "unlock-recipe" and effect.recipe == recipe_name then
         table.remove(tech.effects, i)
+        -- maybe we can break here? not sure if one tech can unlock the same recipe twice
       end
     end
   end
 end
 
--- given a prototype type and name, remove that single prototype
-function remove_one_prototype(type, name)
-  if data.raw[type] then
-    log(serpent.line({type,name,data.raw[type][name] and data.raw[type][name].name or ""}))
-    if data.raw[type][name] then
-      if string.sub(type,1,4) == "item" then -- item or item-with-whatever
-        log("x")
+-- given a prototype category and name, remove that single prototype
+function remove_one_prototype(category, name)
+  if data.raw[category] then
+    if data.raw[category][name] then
+      if string.sub(category,1,4) == "item" then
         -- eliminate recipe->item dependencies
         for recipe_name,recipe in pairs(data.raw.recipe) do
           remove_recipe_ingredient_or_result(recipe, name)
         end
         -- eliminate achievement->item dependencies
-        for _,achievement_type in ipairs({"produce-per-hour-achievement","produce-achievement"}) do
-          for achievement_name,achievement in pairs(data.raw[achievement_type]) do
+        for _,achievement_category in ipairs({"produce-per-hour-achievement","produce-achievement"}) do
+          for achievement_name,achievement in pairs(data.raw[achievement_category]) do
             if achievement.item_product == name then
-              data.raw[achievement_type][achievement_name] = nil
+              data.raw[achievement_category][achievement_name] = nil
             end
           end
         end
         -- eliminate entity->item dependencies
-        for entity_type,_ in pairs(entity_types) do
-          if data.raw[entity_type] then
-            for entity_name,entity in pairs(data.raw[entity_type]) do
-              log(serpent.line({entity_type,entity_name,entity.minable}))
-              if entity.minable and entity.minable.result == name then
-                entity.minable.result = nil
-              end
-              -- if this item places an entity with a blank order, copy it
-              if data.raw[type][name].place_result == entity_name and not entity.order then
-                entity.order = data.raw[type][name].order
-              end
+        for entity_category,entity_prototypes in pairs(data.raw) do
+          for entity_name,entity in pairs(entity_prototypes) do
+            if entity.minable and entity.minable.result == name then
+              -- remove mining results that are this item
+              entity.minable.result = nil
+            end
+            if data.raw[category][name].place_result == entity_name and not entity.order then
+              -- copy item order to blank placed entity order
+              entity.order = data.raw[category][name].order
             end
           end
         end
       end
-      -- game won't start if we leave a type with zero prototypes
-      -- skip the actual delete, but still do all the prerequisite stuff for effect  
-      if table_size(data.raw[type])>1 then
-        data.raw[type][name] = nil
-      end
-      if type == "recipe" then
+      if category == "recipe" then
         -- eliminate technology->recipe dependencies
         for tech_name,tech in pairs(data.raw.technology) do
           remove_technology_unlock(tech, name)
@@ -99,15 +97,61 @@ function remove_one_prototype(type, name)
           end
         end
       end
-      if entity_types[type] then
-        -- eliminate entity->item dependencies
+      if entity_categories[category] then
+        -- eliminate item->entity dependencies
         for item_name,item in pairs(data.raw.item) do
           if item.place_result == name then
             item.place_result = nil
           end
         end
+        -- eliminate achievement->entity dependencies
+        for achievement_name,achievement in pairs(data.raw["build-entity-achievement"]) do
+          if achievement.to_build == name then
+            data.raw[achievement_category][achievement_name] = nil
+          end
+        end
+        for achievement_name,achievement in pairs(data.raw["dont-build-entity-achievement"]) do
+          if type(achievement.dont_build) == "table" then
+            for i,dont_build in ipairs(achievement.dont_build) do
+              if dont_build == name then
+                table.remove(achievement.dont_build, i)
+              end
+            end
+            if #achievement.dont_build == 0 then
+              data.raw["dont-build-entity-achievement"][achievement_name] = nil
+            end
+          else
+            if achievement.dont_build == name then
+              data.raw["dont-build-entity-achievement"][achievement_name] = nil
+            end
+          end
+        end
+        for achievement_name,achievement in pairs(data.raw["dont-use-entity-in-energy-production-achievement"]) do
+          if achievement.excluded then
+            if type(achievement.excluded) == "table" then
+              for i,excluded in ipairs(achievement.excluded) do
+                if excluded == name then
+                  table.remove(achievement.excluded, i)
+                end
+              end
+              if #achievement.excluded == 0 then
+                achievement.excluded = nil
+              end
+            else
+              if achievement.excluded == name then
+                achievement.excluded = nil
+              end
+            end
+          end
+          if achievement.included == name then
+            achievement.included = nil
+          end
+          if not achievement.excluded and not achievement.included then
+            data.raw["dont-use-entity-in-energy-production-achievement"][achievement_name] = nil
+          end
+        end
       end
-      if type == "technology" then
+      if category == "technology" then
         -- eliminate technology->technology dependencies
         for technology_name,technology in pairs(data.raw.technology) do
           if technology.prerequisites then
@@ -125,7 +169,7 @@ function remove_one_prototype(type, name)
           end
         end
       end
-      if type == "trivial-smoke" then
+      if category == "trivial-smoke" then
         -- eliminate artillery-projectile->trivial-smoke dependencies
         for ap_name,ap in pairs(data.raw["artillery-projectile"]) do
           if ap.action and ap.action.action_delivery and ap.action.action_delivery.target_effects then
@@ -137,7 +181,7 @@ function remove_one_prototype(type, name)
           end
         end
       end
-      if type == "tutorial" then
+      if category == "tutorial" then
         -- eliminate tutorial->tutorial dependencies
         for tutorial_name,tutorial in pairs(data.raw.tutorial) do
           if tutorial.dependencies then
@@ -152,28 +196,43 @@ function remove_one_prototype(type, name)
           end
         end
       end
-      -- TODO remove prototypes that have other hard dependencies on this one
+      if category == "resource" then
+        -- eliminate mapgenpreset->resource dependencies
+        for preset_name,preset in pairs(data.raw["map-gen-presets"].default) do
+          if preset.basic_settings and preset.basic_settings.autoplace_controls then
+            for resource,_ in pairs(preset.basic_settings.autoplace_controls) do
+              if resource == name then
+                preset.basic_settings.autoplace_controls[resource] = nil
+              end
+            end
+          end
+        end
+      end
+      -- TODO eliminate additional dependencies
+      if table_size(data.raw[category])>1 then
+        -- delete the prototype, unless that would leave a category empty which is not allowed
+        data.raw[category][name] = nil
+        -- TODO else to blank out as much of the prototype as possible without deleting it
+      end
     end
   end
 end
 
--- given a prototype name or type.name, remove every matching prototype
+-- given a prototype name or category.name, remove every matching prototype
 function remove_prototype(name)
   local dot = string.find(name, ".", 1, true) -- plain no-patterns matching
   if dot then
-    -- specific type.name
+    -- specific category.name
     remove_one_prototype( string.sub(name,1,dot-1) , string.sub(name,dot+1) )
   else
-    -- remove this name of every type
-    for type,prototypes in pairs(data.raw) do
-      remove_one_prototype(type,name)
+    -- remove this name of every category
+    for category,_ in pairs(data.raw) do
+      remove_one_prototype(category,name)
     end
   end
 end
 
 local prototype_list_string = settings.startup['remove-prototypes-prototype-list'].value
-
-log(serpent.line(data.raw.item["artillery-wagon"]))
 
 -- split the list on commas, remove each listed name
 for entry in string.gmatch(prototype_list_string, "[^,]+") do
